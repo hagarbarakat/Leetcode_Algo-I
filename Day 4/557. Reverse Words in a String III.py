72bd0ae78eeecde7707f2b100d0d3964cc8bff73
Hagar Barakat <hagarbarakat97@gmail.com>
2022-02-01 16:51:36 +0200

    Create 557. Reverse Words in a String III.py

---
+++ Day 4/557. Reverse Words in a String III.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        left, right = 0, 0
+        res = []
+        while right < len(s):
+            if s[right] == " ":
+                curr = s[left:right]
+                res.append(curr[::-1])
+                left = right + 1
+            right += 1
+        res.append(s[left:right][::-1])
+        return " ".join(res)
